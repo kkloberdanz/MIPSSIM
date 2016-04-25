facb8116380a4e7a01f4a0d78f5b48e5b14f7726
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-04-25 10:45:17 -0500

    globals no longer extern

--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,23 @@
 #include <cstdint>
 #include <vector>
 
-#include "Headers/globals.h"
 #include "Headers/kernel.h"
 #include "Headers/registers.h"
 #include "Headers/getters.h"
+
+/* initialize registers */
+std::vector<uint32_t> REGISTERS(32, 0x0); 
+
+/* 
+ * Initialize memory 
+ *
+ * Each section of memory is 8 bits wide. 1 word is 32 bits,
+ * so 4 sections of memory make up 1 word.
+ */
+std::vector<uint8_t> MEMORY(1000, 0x0);
+
+/* Increment by 4 after each instruction fetched */
+uint32_t PROGRAM_COUNTER = 0; 
 
 void load_in_memory(uint32_t machinecode) {
     MEMORY[PROGRAM_COUNTER] = machinecode;
